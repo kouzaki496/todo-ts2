0af1200e922755e28b2b790816673fe4446b2c62
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2025-02-11 23:53:01 +0900

    テーマカラーの追加

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -14,6 +14,26 @@ const theme = createTheme({
       light: colors.secondary[200],
       dark: colors.secondary[700],
     },
+    error: {
+      main: colors.error.main,
+      light: colors.error.light,
+      dark: colors.error.dark,
+    },
+    warning: {
+      main: colors.warning.main,
+      light: colors.warning.light,
+      dark: colors.warning.dark,
+    },
+    info: {
+      main: colors.info.main,
+      light: colors.info.light,
+      dark: colors.info.dark,
+    },
+    success: {
+      main: colors.success.main,
+      light: colors.success.light,
+      dark: colors.success.dark,
+    },
   },
   typography: {
     fontFamily: 'Roboto, sans-serif',
