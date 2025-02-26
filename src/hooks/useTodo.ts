baa933d730e32ae8901aa050fba8ce8c89ff858f
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2025-02-26 22:59:42 +0900

    いらないかも。テスト用として残す？

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -1,41 +1,45 @@
-//src/hooks/useTodo.ts
-import { useState } from 'react';
-import Todo from '../types/todo';
+// //src/hooks/useTodo.ts
+// ローカルの状態管理のみ
+// データベースとの同期は行わない
+// テスト用として残す？
 
-export const useTodo = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+// import { useState } from 'react';
+// import Todo from '../types/todo';
 
-  //追加処理
-  const addTodo = (title:string, dueDate:string) => {
-    const newTodo:Todo = {
-      id:Date.now(),
-      title,
-      completed:false,
-      dueDate,
-      selected: false,
-    }
-    setTodos(prevTodos => [...prevTodos, newTodo]);
-  };
+// export const useTodo = () => {
+//   const [todos, setTodos] = useState<Todo[]>([]);
 
-  //削除処理
-  const deleteTodo = (id:number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+//   //追加処理
+//   const addTodo = (title:string, dueDate:string) => {
+//     const newTodo:Todo = {
+//       id:Date.now(),
+//       title,
+//       completed:false,
+//       dueDate,
+//       selected: false,
+//     }
+//     setTodos(prevTodos => [...prevTodos, newTodo]);
+//   };
 
-  //更新処理
-  const updateTodo = (id:number, updatedFields:Partial<Todo>) => {
-    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, ...updatedFields } : todo)));
-  };
+//   //削除処理
+//   const deleteTodo = (id:number) => {
+//     setTodos(todos.filter((todo) => todo.id !== id));
+//   };
 
-  return {
-    todos,
-    addTodo,
-    deleteTodo,
-    updateTodo
-  };
-};
+//   //更新処理
+//   const updateTodo = (id:number, updatedFields:Partial<Todo>) => {
+//     setTodos(todos.map((todo) => (todo.id === id ? { ...todo, ...updatedFields } : todo)));
+//   };
 
-export default useTodo;
+//   return {
+//     todos,
+//     addTodo,
+//     deleteTodo,
+//     updateTodo
+//   };
+// };
+
+// export default useTodo;
 
 
 
